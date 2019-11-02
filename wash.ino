8b1eba97c2e32773a8dbc96a1fd526c6709e7657
Виктор Антонович <bvictor@belous.su>
2019-11-02 20:41:23 +0300

    compiled

--- wash.ino
+++ wash.ino
@@ -1,14 +1,14 @@
 // Программа для бойлера в ванной
 // Пока она вручную открывает и закрывает вентили и
 // включает и выключает бойлер
-const int S-cold = 13;	          //  Switch cold pin
-const int S-hot = 12;	          // Switch hot pin
-const int S-boil = 11;	          // Switch boiler pin
-const int S-auto = 10;	          // Switch auto/manual pin
-const int L-cold = 9;	          // Led cold pin
-const int L-hot = 8;	          // Led hot pin
-const int L-boil = 7;	          // Led boiler pin
-const int L-auto = 6;	          // Led auto pin
+const int Scold = 13;	          //  Switch cold pin
+const int Shot = 12;	          // Switch hot pin
+const int Sboil = 11;	          // Switch boiler pin
+const int Sauto = 10;	          // Switch auto/manual pin
+const int Lcold = 9;	          // Led cold pin
+const int Lhot = 8;	          // Led hot pin
+const int Lboil = 7;	          // Led boiler pin
+const int Lauto = 6;	          // Led auto pin
 const int RelCold = 5;	          // Relay cold 1 pin
 const int RelHot = 4;	          // Relay hot 2 pin
 const int RelBoil = 3;	          // Relay boiler 3 pin
@@ -16,7 +16,7 @@ const int RelOn = 2;	          // Relay power on 4 pin
 const int PressMin = 100;	  // Pressure min
 unsigned long DelaySleep = 600000; // 10 min test cicle
 int PressCold;			  // Pressure cold variable
-int pressHot;			  // Pressure hot variable
+int PressHot;			  // Pressure hot variable
 int SenCold = 0;		  //  Sensor cold A0 pin
 int SenHot = 1;			  // Sensor hot A1 pin
 int DelaySwitch = 10000;	  //  Время для переключения вентиля
@@ -29,22 +29,22 @@ int AutoStatus = 1;			  // Status auto
 // ?3 - ????? ???????? ??????? ????
 void setup ()
 {
-	pinMode(S-cold, INPUT_PULLUP);    // Switch cold input pullup pin
-	pinMode(S-hot, INPUT_PULLUP);	  // Switch hot input pullup pin
-	pinMode(S-boil, INPUT_PULLUP);	  // Switcch boiler input pullup pin
-	pinMode(S-auto, INPUT_PULLUP);	  // Switch auto input pullup pin
-	pinMode(L-cold, OUTPUT);	  // Cold led pin
-	pinMode(L-hot, OUTPUT);	          // Hot led pin
-	pinMode(L-boil, OUTPUT);	  // Boil led pin
-  pinMode(L-auto, OUTPUT);	          // Auto led pin
+	pinMode(Scold, INPUT_PULLUP);    // Switch cold input pullup pin
+	pinMode(Shot, INPUT_PULLUP);	  // Switch hot input pullup pin
+	pinMode(Sboil, INPUT_PULLUP);	  // Switcch boiler input pullup pin
+	pinMode(Sauto, INPUT_PULLUP);	  // Switch auto input pullup pin
+	pinMode(Lcold, OUTPUT);	  // Cold led pin
+	pinMode(Lhot, OUTPUT);	          // Hot led pin
+	pinMode(Lboil, OUTPUT);	  // Boil led pin
+        pinMode(Lauto, OUTPUT);	          // Auto led pin
 	pinMode(RelCold, OUTPUT);	  // Cold relay pin
 	pinMode(RelHot, OUTPUT);	  // Hot relay pin
 	pinMode(RelBoil, OUTPUT);	  // Boil relay pin
 	pinMode(RelOn, OUTPUT);		  // Power relay pin
-	digitalWrite(L-cold, LOW);	  // Cold led pin off
-	digitalWrite(L-hot, LOW);	  // Hot led pin off
-	digitalWrite(L-boil, LOW);	  // Boiler led pin off
-	digitalWrite(L-auto, LOW);	  // Auto led pin off
+	digitalWrite(Lcold, LOW);	  // Cold led pin off
+	digitalWrite(Lhot, LOW);	  // Hot led pin off
+	digitalWrite(Lboil, LOW);	  // Boiler led pin off
+	digitalWrite(Lauto, LOW);	  // Auto led pin off
 	digitalWrite(RelCold, LOW);	  // Cold relay off
 	digitalWrite(RelHot, LOW);	  // Hot relay off
 	digitalWrite(RelBoil, LOW);	  // Boil relay off
@@ -58,14 +58,14 @@ void setup ()
 	if (PressCold > PressMin)	  // if cold water
 	{
 		digitalWrite(RelCold, HIGH);	// open cold water
-		digitalWrite(L-cold, HIGH);	// light cold led
+		digitalWrite(Lcold, HIGH);	// light cold led
 		Cold = 1;			// cold water yes
 	}
 	PressHot = analogRead(SenHot);		// read hot pressure
 	if (PressHot > PressMin)		// if hot water
 	{
 		digitalWrite(RelHot, HIGH);	// open hot water
-		digitalWrite(L-hot, HIGH);	// light hot led
+		digitalWrite(Lhot, HIGH);	// light hot led
 		Hot = 1;			// hot water yes
 	}
 	else					// no hot water
@@ -73,12 +73,12 @@ void setup ()
 		if (Cold == 1)			// cold water yes
 		{
 			digitalWrite(RelBoil, HIGH);	// switch on boiler
-			digitalWrite(L-boil, HIGH);	// light led boiler
+			digitalWrite(Lboil, HIGH);	// light led boiler
 			Boiler = 1;			// boiler yes
 		}
 	}
 	delay(DelaySwitch);                     // wait to switch water
-	digitalWrite(L-auto, HIGH);		// light led auto
+	digitalWrite(Lauto, HIGH);		// light led auto
 	digitalWrite(RelOn, LOW);		// switch off relay power
 }
 void loop ()
@@ -102,9 +102,9 @@ void loop ()
 		{
 			Hot = 1;			// Hot change from no to yes
 			digitalWrite(RelHot,HIGH);	// Open Hot water
-			digitalWrite(L-hot,HIGH);	// light hot led
+			digitalWrite(Lhot,HIGH);	// light hot led
 			digitalWrite(RelBoil, HIGH);	// boiler relay on
-			digitalWrite(L-boil, HIGH);	// boiler led on
+			digitalWrite(Lboil, HIGH);	// boiler led on
 			Boiler = 1;			// boiler on
 			digitalWrite(RelOn,HIGH);	// power on relay
 			delay(DelaySwitch);		// wait for switch water
@@ -114,9 +114,9 @@ void loop ()
 		{
   			Hot = 0;			// Hot change from yes to no
 			digitalWrite(RelHot,LOW);	// Hot close
-			digitalWrite(L-hot,LOW);	// down hot led
+			digitalWrite(Lhot,LOW);	// down hot led
 			digitalWrite(RelBoil, LOW);	// boiler relay off
-			digitalWrite(L-boil, LOW);	// boiler led off
+			digitalWrite(Lboil, LOW);	// boiler led off
 			Boiler = 0;			// boiler off
 			digitalWrite(RelOn,HIGH);	// power on relay
 			delay(DelaySwitch);		// wait for switch water
@@ -126,54 +126,54 @@ void loop ()
 	}
 	if (AutoStatus == 0) // manual control
 	{
-		if (Cold == 0 && S-cold == HIGH)        // switch on cold water
+		if (Cold == 0 && Scold == HIGH)        // switch on cold water
 		{
 			digitalWrite(RelCold, HIGH);	// cold relay on
-			digitalWrite(L-cold, HIGH);	//cold led on
+			digitalWrite(Lcold, HIGH);	//cold led on
 			Cold = 1;			// cold on
 			digitalWrite(RelOn, HIGH);	// power on relay
 			delay(DelaySwitch);		// wait for swich water
-			digitalWrite(RelOn, LOW):	// power off relay
+			digitalWrite(RelOn, LOW);	// power off relay
 		}
-		if (Cold == 1 && S-cold == LOW)	        // swich off cold water
+		if (Cold == 1 && Scold == LOW)	        // swich off cold water
 		{
 			digitalWrite(RelCold, LOW);	// cold relay off
-			digitalWrite(L-cold, LOW);	// cold led off
+			digitalWrite(Lcold, LOW);	// cold led off
 			Cold = 0;			// cold off
 			digitalWrite(RelOn, HIGH);	// power on relay
 			delay(DelaySwitch);		// wait for switch water
 			digitalWrite(RelOn, LOW);	// power off relay
 		}
-		if (Hot == 0 && S-hot == HIGH)		// switch on hot water
+		if (Hot == 0 && Shot == HIGH)		// switch on hot water
 		{
 			digitalWrite(RelHot, HIGH);	// hot relay on
-			digitalWrite(L-Hot, HIGH);	// hot led on
+			digitalWrite(Lhot, HIGH);	// hot led on
 			Hot = 1;			// hot on
 			digitalWrite(RelOn, HIGH);	// power on relay
 			delay(DelaySwitch);		// wait for switch water
 			digitalWrite(RelOn, LOW);	// power off relay
 		}
-		if (Boiler == 0 && S-boil == HIGH);	// switch on boiler
+		if (Boiler == 0 && Sboil == HIGH);	// switch on boiler
 		{
 			digitalWrite(RelBoil, HIGH);	// switch on boiler
-			digitalWrite(L-Boil, HIGH);	// switch on led boiler
+			digitalWrite(Lboil, HIGH);	// switch on led boiler
 			Boiler = 1;			// Boiler on
 		}
-		if (Boiler == 1 && S-boil == LOW);	// switch off boiler
+		if (Boiler == 1 && Sboil == LOW);	// switch off boiler
 		{
 			digitalWrite(RelBoil, LOW);	// switch off boiler
-			digitalWrite(L-Boil, LOW);	// switch off led boiler
+			digitalWrite(Lboil, LOW);	// switch off led boiler
 			Boiler = 0;
 		}
 	}
-	if (AutoStatus == 0 && S-auto == HIGH) 		// switch auto on
+	if (AutoStatus == 0 && Sauto == HIGH) 		// switch auto on
 	{
-		digitalWrite(L-auto, HIGH);		// switch auto led on
+		digitalWrite(Lauto, HIGH);		// switch auto led on
 		AutoStatus = 1;				// switch auto on
 	}
-	if (AutoStatus == 1 && S-auto == LOW)			// switch auto off
+	if (AutoStatus == 1 && Sauto == LOW)			// switch auto off
 	{
-		digitalWrite(L-auto, LOW);		// switch auto led off
+		digitalWrite(Lauto, LOW);		// switch auto led off
 		AutoStatus = 0;
 	}
 }
